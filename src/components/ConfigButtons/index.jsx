7ba93b00684c68915ee0359937542d01a1c35308
Santiago urrego <santiagourregolaya@gmail.com>
2025-12-04 14:41:10 -0500

    Implementation of role change by the administrator

--- src/components/ConfigButtons/index.jsx
+++ src/components/ConfigButtons/index.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from '../../store/slices/userSlice';
+import { selectIsAdmin } from '../../store/slices/userSelectors';
 import ChangePasswordModal from '../ChangePasswordModal';
+import ManageUsersModal from '../ManageUsersModal';
 import './ConfigButtons.scss';
 
 const ConfigButtons = () => {
   const dispatch = useDispatch();
+  const isAdmin = useSelector(selectIsAdmin);
   const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false);
+  const [isManageUsersModalOpen, setIsManageUsersModalOpen] = useState(false);
 
   const handleLogout = () => {
     dispatch(logoutUser());
@@ -15,14 +19,27 @@ const ConfigButtons = () => {
   return (
     <div className="config-buttons">
       <h2>Configuración</h2>
+      
+      {isAdmin && (
+        <button onClick={() => setIsManageUsersModalOpen(true)}>
+          Gestionar Usuarios
+        </button>
+      )}
+      
       <button onClick={() => setIsPasswordModalOpen(true)}>
         Cambiar Contraseña
       </button>
+      
       <button onClick={handleLogout}>Cerrar Sesión</button>
       
       <ChangePasswordModal
         isOpen={isPasswordModalOpen}
         onClose={() => setIsPasswordModalOpen(false)}
+      />
+      
+      <ManageUsersModal
+        isOpen={isManageUsersModalOpen}
+        onClose={() => setIsManageUsersModalOpen(false)}
       />
     </div>
   );
