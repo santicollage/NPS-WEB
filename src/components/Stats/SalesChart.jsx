cb1390a91c733778f2a9f03c28799c9a14b9b7a6
Santiago urrego <santiagourregolaya@gmail.com>
2025-12-17 19:24:39 -0500

    Fix basic statistics

--- src/components/Stats/SalesChart.jsx
+++ src/components/Stats/SalesChart.jsx
@@ -76,7 +76,7 @@ const SalesChart = ({ data, loading, onPeriodChange, onDateRangeChange }) => {
       </div>
 
       <div className="chart-wrapper">
-        <ResponsiveContainer>
+        <ResponsiveContainer width="100%" height={300} debounce={1} minWidth={0}>
           <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
             <defs>
               <linearGradient id="colorSales" x1="0" y1="0" x2="0" y2="1">
